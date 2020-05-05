3041f7ab2b81522f8529f793a9e88500bbf08597
root <ddani1995@gmail.com>
2020-05-05 13:15:35 +0200

    added dunno response

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ fs.readdir("./events/", (err, files) => {
   });
 });
 
+setInterval(function() {
+  let j = {};
+  client.guilds.forEach((item, index) =>{
+    item.members.forEach((member, key, map) =>{
+      if(member.presence.status != "offline" && member.presence.status != "dnd" && !member.user.bot ){
+        id =member.user.avatarURL.replace("=2048", "=16"); 
+        j[id] = member.presence.status;
+        
+      }
+    })
+  });
+  console.log(j);
+    fs.writeFile("/var/www/html/discord/index.php", JSON.stringify(j, null, 2) , (err) => {
+      if(err) console.log(err);
+    });
+},600000, null);
+
 client.login(process.env.BOT_TOKEN);
