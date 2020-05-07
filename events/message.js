3bd6b732dd72bf0c0e116ad10195ee5cec5afc56
root <ddani1995@gmail.com>
2020-05-07 20:32:11 +0200

    bug fix, minecraft player count status, gif reply

--- events/message.js
+++ events/message.js
@@ -1,7 +1,34 @@
 const kick = require("../commands/kick");
-
+const shell = require("shelljs");
+const fs = require("fs");
+const {client, Attachment } = require('discord.js');
 module.exports = (client, message) => {
+  let m =  message.content.toLowerCase();
   if (message.content.startsWith("!kick")) {
     return kick(message);
+  }
+  if (m == ("ip")){
+    message.reply(""+(shell.exec("myip")));
+  }
+  if (m == "dunno"){
+    message.delete();
+    message.reply("¯\\_(ツ)_/¯");
+  }
+  if (message.content == ":A"){
+    message.delete();
+    message.reply("( ͡° ͜ʖ ͡°)");
+  }
+  console.log(m);
+  console.log(m.split(" ").length);
+  if (m.split(" ").length == 1){
+    console.log("asd");
+    ["gif", "jpeg", "jpg", "png"].forEach(e =>
+    {
+      console.log(e);
+      if (fs.existsSync("/stuff/" + m + '.' + e)){
+        message.delete();
+        message.reply(new Attachment("/stuff/" + m + "." + e));
+      }
+    });
   }
 };
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
+const ms = require("./minestat");
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir("/stuff/discord-bot/events/", (err, files) => {
   files.forEach(file => {
-    const eventHandler = require(`./events/${file}`);
+    const eventHandler = require(`/stuff/discord-bot/events/${file}`);
     const eventName = file.split(".")[0];
     client.on(eventName, (...args) => eventHandler(client, ...args));
   });
@@ -13,12 +14,15 @@ fs.readdir("./events/", (err, files) => {
 
 setInterval(function() {
   let j = {};
+  let players = 0;
+  ms.init('192.168.0.59', 25565, function(result){ if(ms.online){ players = ms.current_players;}else{players = 'OFFLINE'} });
+  client.user.setActivity('MC:'+players,{ type: 0 }).then(console.log)
+  console.log(client.user);
   client.guilds.forEach((item, index) =>{
     item.members.forEach((member, key, map) =>{
-      if(member.presence.status != "offline" && member.presence.status != "dnd" && !member.user.bot ){
-        id =member.user.avatarURL.replace("=2048", "=16"); 
+      if(member.presence.status != "offline" && member.presence.status != "dnd" && !member.user.bot && member.user.avatarURL != null){
+        id =member.user.avatarURL.replace("=2048", "=16");
         j[id] = member.presence.status;
-        
       }
     })
   });
